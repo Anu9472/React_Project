8ccac4a382cfcf1972b3e1f09e2270a75e20859e
Anushka Kale <anushkakale35124@gmail.com>
2026-01-15 18:39:20 +0530

    Components(Src)

---
+++ Hero.jsx
@@ -0,0 +1,45 @@
+import React from "react"
+
+const Hero = ()=>{
+    return(
+        <main className="hero">
+          <div className="hero-content">
+<h1> YOUR FEET DESERVE THE BEST </h1>
+<p> YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR SHOES.
+    YOUR FEET DESERVE THE BEST AND WE'RE HERE TO HELP YOU WITH OUR SHOES.    
+</p>
+
+<div className="Hero-btn">  
+    <button>Shop Now</button>
+    <button>Category</button>
+</div>
+
+<div className="shopping">
+    <p> Also Avaliable On </p>
+
+<div className="brand-icons">
+<img src="https://th.bing.com/th/id/OIP.WG7YGavkfToGLEniTpcKeAHaHa?w=161&h=180&c=7&r=0&o=7&pid=1.7&rm=3" alt="amazon-logo" />
+<img src="https://th.bing.com/th/id/OIP.KWHq6ElgPUTTi222ZaX-ZAHaHa?w=153&h=180&c=7&r=0&o=7&pid=1.7&rm=3" alt="Flipkart-logo" />
+
+</div>
+
+
+
+
+</div>
+
+
+
+
+          </div>
+          <div className="hero-image">
+
+          <img src="https://i.ebayimg.com/images/g/IUYAAOSwK6FnD9pG/s-l1200.jpg" alt="hero-image" />
+
+
+          </div>
+        </main>
+    )
+}
+
+export default Hero
