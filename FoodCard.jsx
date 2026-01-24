83076612d58f5b419a70cef802820b9cc231a59a
Anushka Kale <anushkakale35124@gmail.com>
2026-01-24 14:42:45 +0530

    Component(

--- FoodCard.jsx
+++ FoodCard.jsx
@@ -1,24 +1,23 @@
-import React , {useContext} from "react";
-import { CartContext } from "../Context/CartContex";
+import React, { useContext } from "react";
+import { CartContext } from "../Context/CartContext";
 
-const FoodCard = ({item})=>{
+const FoodCard = ({ item, cart, setCart }) => {
 
-const { setCartCount } = useContext(CartContext)
+  const { setCartCount } = useContext(CartContext);
 
+  function addToCart() {
+    const updatedCart = [...cart, item];
+    setCart(updatedCart);                 // update cart
+    setCartCount(updatedCart.length);     // update navbar count
+  }
 
-function addToCart(){
-     
-}
-
-return(
-    <div style={{border:"2px solid #ccc", margin:10, padding:10}}>
-        <h4>{item.name}</h4>
-        <h5>{item.price}</h5>
-        <button onClick={addToCart}>Add to Cart</button>
-
+  return (
+    <div style={{ border: "2px solid #ccc", margin: 10, padding: 10 }}>
+      <h4>{item.name}</h4>
+      <h5>₹{item.price}</h5>
+      <button onClick={addToCart}>Add to Cart</button>
     </div>
-)
+  );
+};
 
-
-
-}
+export default FoodCard;
