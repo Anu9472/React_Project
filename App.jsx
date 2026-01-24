222361e950803dd8b57758629f2bc3281624eca7
Anushka Kale <anushkakale35124@gmail.com>
2026-01-24 14:45:15 +0530

    src

--- App.jsx
+++ App.jsx
@@ -1,16 +1,40 @@
-// import "./App.css"
-import React from "react"
-import Hero from "./Components/Hero"
-import Navigation from "./Components/Navigation"
+import { useState } from 'react'
+import Navbar from './Components/Navbar'
+import { CartProvider } from "./Context/CartContext";
+import Foodlist from './Components/Foodlist'
 
-const App = ()=>{
-  return(
-    <div>
-      <Navigation/>
-      <Hero/>
-    </div>
-  )
+
+
+
+
+const FoodData = [
+  {id:1,name:"Pizza",price:250},
+  {id:2,name:"Burger",price:110},
+  {id:3,name:"pavbaji",price:80},
+  {id:4,name:"Pasta",price:120}
+]
+
+
+
+function App() {
+
+const[ cart,setCart] = useState([]);
+
+
+
+
+
+
+
+
+return(
+<CartProvider>
+<Navbar/>
+<Foodlist FoodData={FoodData} cart= {cart} setCart= {setCart}/>
+</CartProvider>
+
+
+)
 }
-
 
 export default App
